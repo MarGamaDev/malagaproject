9d054a1d92a7d5d0583a290e7d39246e191f9917
MarGama <210358@student.glu.nl>
2023-01-29 17:09:03 +0100

    very small start on project

---
+++ Malaga Project/Assets/Assets/Scripts/ProfileData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct ProfileData
+{
+    public int Id;
+    public string Name;
+    public int AvailableCoin;
+    //public SubscriptionTier SubTier;
+
+}
